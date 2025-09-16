04f7cfba442d849d40cb956cba9f2afa83783528
ArdaUtu <9024283@student.zadkine.nl>
2025-09-16 09:30:36 +0200

    Opdracht 2 klaar

--- 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
+++ 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
@@ -8,17 +8,17 @@ console.log('Temperaturen:', temperaturen);
 console.log('Kleuren:', kleuren);
 
 // TODO: Zoek de eerste temperatuur boven 25 graden
-const warmeTemperatuur = /* jouw code hier */
+const warmeTemperatuur = temperaturen.find(persoon => persoon.temperaturen > 25); /* jouw code hier */
 console.log('Eerste warme temperatuur:', warmeTemperatuur);
 
 // TODO: Zoek de index van de kleur 'groen'
-const groenIndex = /* jouw code hier */
+const groenIndex = kleuren.findIndex(kleur => kleur ==='groen'); /* jouw code hier */
 console.log('Index van groen:', groenIndex);
 
 // TODO: Zoek de eerste temperatuur onder 20 graden
-const koudeTemperatuur = /* jouw code hier */
+const koudeTemperatuur = temperaturen.find(persoon => persoon.temperaturen < 20); /* jouw code hier */
 console.log('Eerste koude temperatuur:', koudeTemperatuur);
 
 // TODO: Zoek de index van de eerste temperatuur boven 23 graden
-const warmeIndex = /* jouw code hier */
+const warmeIndex = temperaturen.findIndex(temperatuur => temperatuur > 23); /* jouw code hier */
 console.log('Index van eerste warme temperatuur:', warmeIndex);
