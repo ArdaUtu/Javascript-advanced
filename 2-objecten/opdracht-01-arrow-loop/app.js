b59cca06efcf2a2b8a9a67d7dfe5ea35a8924889
ArdaUtu <9024283@student.zadkine.nl>
2025-09-23 12:00:06 +0200

    Opdracht 1 klaar

--- 2-objecten/opdracht-01-arrow-loop/app.js
+++ 2-objecten/opdracht-01-arrow-loop/app.js
@@ -1,4 +1,19 @@
 // Je gaat even oefenen met het gebruiken van een arrow function en de for/of loop. 
+const marvels = [
+    "iron man 2008", "The avengers 2012", "Avengers endgame 2019", "black panther 2018"
+];
+for (let film of marvels){
+    console.log(film);
+}
+
+const addMarvel = (film) => {
+return marvels.push(film);
+};
+addMarvel("Iron man 2019");
+console.log(marvels);
+
+
+
 
 // Maak een array genaamd 'marvels' en plaats hier 4 Marvel films in.
 
