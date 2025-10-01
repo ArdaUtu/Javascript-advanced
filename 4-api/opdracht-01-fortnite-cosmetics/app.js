141560caf11383affd91804c7ce4878720dff92d
ArdaUtu <9024283@student.zadkine.nl>
2025-10-01 10:29:16 +0200

    Klaar

---
+++ 4-api/opdracht-01-fortnite-cosmetics/app.js
@@ -0,0 +1,31 @@
+fetch('https://fortnite-api.com/v2/cosmetics/new')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('cosmetics');
+    container.classList.add('cosmetics-container'); // CSS class toevoegen
+ 
+    if (data.data && data.data.items && Array.isArray(data.data.items.br)) {
+      if (data.data.items.br.length === 0) {
+        container.innerHTML = "<p>Geen nieuwe cosmetics gevonden.</p>";
+      } else {
+        for (let cosmetic of data.data.items.br) {
+          const div = document.createElement('div');
+          div.classList.add('cosmetic-card'); // CSS class toevoegen
+ 
+          div.innerHTML = `
+            <h3>${cosmetic.name}</h3>
+            <p>Type: ${cosmetic.type.displayValue}</p>
+            <img src="${cosmetic.images.icon}" alt="${cosmetic.name}" width="100">
+          `;
+          container.appendChild(div);
+        }
+      }
+    } else {
+      container.innerHTML = "<p>Geen nieuwe cosmetics gevonden.</p>";
+    }
+  })
+  .catch(error => {
+    console.log('Fout:', error);
+    const container = document.getElementById('cosmetics');
+    container.innerHTML = '<p>Sorry, de cosmetics konden niet geladen worden.</p>';
+  });
