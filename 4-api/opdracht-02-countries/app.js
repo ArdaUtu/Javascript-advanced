65b5932ae32973c6ce844f4b221e5e367c98dc69
ArdaUtu <9024283@student.zadkine.nl>
2025-10-01 12:08:55 +0200

    KLAAR

--- 4-api/opdracht-02-countries/app.js
+++ 4-api/opdracht-02-countries/app.js
@@ -1,1 +1,18 @@
 //maak een functie over car
+let output = document.querySelector('#CarBody')
+
+
+fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+.then((response) => response.json())
+.then((data) => {
+    console.log(data);
+    for (let item of data){
+        output.innerHTML += `
+             
+            <h3 class="name">${item.name.common}</h3>
+            <img class="image" src=${item.flags.png}>
+            
+        `;
+    }
+
+});
